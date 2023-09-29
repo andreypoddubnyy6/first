887fee1c7e8e2610a9f88b5aff5e1b454d7b087b
andreypoddubnyy6 <146453872+andreypoddubnyy6@users.noreply.github.com>
2023-09-29 18:04:16 +0400

    video app

---
+++ VideoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Oleg Savelyev on 17.04.2022.
+//
+
+import SwiftUI
+
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
